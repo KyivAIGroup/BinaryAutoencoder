aa00ed8dafadd4d7e9493dd7dc9795ec5c8d235f
Viacheslav Osaulenko <hronos7@gmail.com>
2021-07-04 17:41:48 +0300

    added association memory experiment

--- public/fig5b.py
+++ public/fig5b.py
@@ -1,96 +1,190 @@
+# Test accuracy of ass memory for different sprasity of Y layer
+
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.special import comb
-from scipy.stats import norm, binom
-
 import matplotlib as mpl
-
+import itertools
+import matplotlib as mpl
+from scipy.special import comb
 
 mpl.rcParams['grid.color'] = 'k'
 mpl.rcParams['grid.linestyle'] = ':'
 mpl.rcParams['grid.linewidth'] = 0.5
 
-mpl.rcParams['figure.figsize'] = [8.0, 6.0]
+mpl.rcParams['figure.figsize'] = [10.0, 8.0]
 mpl.rcParams['figure.dpi'] = 80
-mpl.rcParams['savefig.dpi'] = 200
+mpl.rcParams['savefig.dpi'] = 800
 mpl.rcParams['savefig.format'] = 'pdf'
 
-mpl.rcParams['font.size'] = 18
-mpl.rcParams['legend.fontsize'] = 18
+mpl.rcParams['font.size'] = 22
+mpl.rcParams['legend.fontsize'] = 22
 mpl.rcParams['figure.titlesize'] = 14
 
-# np.random.seed(0)
+########################
+def kWTA(cells, k, argsorted=None):
+    # n_active = max(int(sparsity * cells.size), 1)
+    if argsorted is not None:
+        winners = argsorted[-k:]
+    else:
+        winners = np.argsort(cells)[-k:]
+    sdr = np.zeros(cells.shape, dtype=cells.dtype)
+    sdr[winners] = 1
+    return sdr
 
-# Overlap
-def intersection(k, a_x, a_w, N):
-    return comb(a_x, k) * comb(N - a_x, a_w - k) / comb(N, a_w)
+def generate_random_vector(N, a_x):
+    vector = np.zeros(N, dtype=int)
+    ones = np.random.choice(N, size=a_x, replace=False)
+    vector[ones] = 1
+    return vector
 
-
-def get_errors(a_x, N_x, a_y, N_y, a_w):
-    max_overlap = np.min([a_x, a_w])
-    overlap_range = np.arange(max_overlap + 1)
-    overlap = np.zeros(max_overlap + 1)
-    for i, k in enumerate(overlap_range):
-        overlap[i] = comb(a_x, k) * comb(N_x - a_x, a_w - k) / comb(N_x, a_w) * N_y
-    threshold = np.nonzero(np.cumsum(overlap) > N_y - a_y)[0][0]
-    # max_overlap = np.where(overlap > 0.0001)[0][-1]  # this is to speed up the following computations
-    # plt.plot(overlap_range, overlap)
-    # plt.axvline(threshold, color='red')
-    # plt.show()
-    # quit()
-    mean_range = np.arange(threshold, max_overlap+1)
-
-    residual = ((N_y - a_y) - np.cumsum(overlap)[threshold - 1])
-    # residual = 0
-
-    mean_ones = 0
-    variance_ones = 0
-    mean_zeros = 0
-    variance_zeros = 0
-    for i, o in enumerate(mean_range):
-        if o == threshold:
-            r = residual
-        else:
-            r = 0
-        mean_ones += float(o) / a_x * (overlap[o] - r)
-        variance_ones += float(o) / a_x * (1 - float(o) / a_x) * (overlap[o] - r)
-        mean_zeros += float(a_w - o) / (N_x - a_x) * ((overlap[o] - r))
-        variance_zeros += float(a_w - o) / (N_x - a_x) * (1 - float(a_w - o) / (N_x - a_x)) * ((overlap[o] - r))
-
-    x_axis = np.linspace(-20, 300, 100000)
-    pdf_0 = norm.pdf(x_axis, mean_zeros, np.sqrt(variance_zeros)) * (N_x - a_x)
-    pdf_1 = norm.pdf(x_axis, mean_ones, np.sqrt(variance_ones)) * a_x
-    sum_pdf = pdf_0 + pdf_1
-    thresh_x_index = np.nonzero(np.cumsum(sum_pdf) * (x_axis[10] - x_axis[9])  >= N_x - a_x)[0][0]
-    thresh_x = x_axis[thresh_x_index]
-    plt.fill_between(x_axis[thresh_x_index:], pdf_0[thresh_x_index:],
-             facecolor="none", edgecolor="k", hatch="-", rasterized=True)
-    plt.fill_between(x_axis[:thresh_x_index], pdf_1[:thresh_x_index],
-                      facecolor="none",edgecolor="k", hatch='|', rasterized=True)
-    plt.fill_between(x_axis[thresh_x_index:], pdf_1[thresh_x_index:],
-            facecolor="none", edgecolor="k", rasterized=True)
-    plt.fill_between(x_axis[:thresh_x_index], pdf_0[:thresh_x_index],
-             facecolor="none", edgecolor="k", rasterized=True)
-
-    plt.plot(x_axis, pdf_0)
-    plt.plot(x_axis, pdf_1)
-    plt.axvline(thresh_x, color='orange')
-    plt.xlim([10, 40])
-    plt.ylim([0, 4])
-    plt.xlabel(r'$k$, inverse  overlap')
-    plt.ylabel(r'$p(v_i = k)$')
-    plt.savefig('figures/two_gauss', bbox_inches='tight')
-    plt.show()
-
-
+def generate_random_matrix(R, N, a_x):
+    matrix = np.zeros((R, N), dtype=int)
+    for i in range(R):
+        matrix[i] = generate_random_vector(N, a_x)
+    return matrix
 
 
 N_x = 50
-N_y = 200
-a_x = 20
-a_w = 30
-a_y = 40
-get_errors(a_x, N_x, a_y, N_y, a_w)
+N_z = 21
+N_y = 2000
+
+a_x = int(N_x * 0.5)
+a_z = int(N_z * 0.5)
+# print(a_z)
+a_w = 7
 
 
+
+import time
+t = time.time()
+
+
+def plot_curve(R, noise=0.0):
+    iters = 5
+    N_split = 50
+    a_y_range = np.hstack([np.arange(1, N_split, 1), np.arange(N_split, N_y, int(N_y*0.01))])
+    data = np.zeros((iters, a_y_range.size))
+    data2 = np.zeros((iters, a_y_range.size))
+    for it in range(iters):
+        print(it)
+        D_x = np.zeros((R, N_x), dtype=int)
+        D_z = np.zeros((R, N_z), dtype=int)
+        for i in range(R):
+            inds = np.random.choice(N_x, size=a_x, replace=False)
+            inds2 = np.random.choice(N_z, size=a_z, replace=False)
+            D_x[i, inds] = 1
+            D_z[i, inds2] = 1
+
+        for k, ayi in enumerate(a_y_range):
+            # print(ayi)
+            D_y = np.zeros((R, N_y), dtype=int)
+            w = generate_random_matrix(N_y, N_x, a_w)
+            w_yz = np.zeros((N_z, N_y))
+            for i in range(R):
+                D_y[i] = kWTA(w @ D_x[i], ayi)
+                w_yz += np.outer(D_z[i], D_y[i])
+
+
+            w_yz[w_yz > 0] = 1
+
+            #Recall
+            error = np.zeros(R)
+            max_error = 2 * a_z * (1 - float(a_z) / N_z) # for random retrieval
+            # print(max_error)
+            for i in range(R):
+                if noise > 0:
+                    z = kWTA(w_yz @ (D_y[i] ^ np.random.binomial(1, noise, size=N_y)), a_z)
+                else:
+                    z = kWTA(w_yz @ D_y[i], a_z)
+                error[i] = np.sum(np.abs(D_z[i] - z)) / (max_error)
+
+            data[it, k] = np.mean(error)
+    if noise > 0:
+        plt.plot(a_y_range / N_y, np.mean(data, axis=0), '-', label=f'R={R} with expansion, noise')
+    else:
+        plt.plot(a_y_range / N_y, np.mean(data, axis=0), '-', label=f'R={R} with expansion')
+    print(f'R={R}')
+    print('min error', np.min(np.mean(data, axis=0)))
+    print('a_min', a_y_range[np.argmin(np.mean(data, axis=0))])
+    print('s_min', a_y_range[np.argmin(np.mean(data, axis=0))] / N_y)
+
+
+
+def plot_curve_both(R):
+    iters = 5
+    N_split = 50
+    a_y_range = np.hstack([np.arange(1, N_split, 1), np.arange(N_split, N_y, int(N_y*0.01))])
+    data = np.zeros((iters, a_y_range.size))
+    data2 = np.zeros((iters, a_y_range.size))
+    for it in range(iters):
+        print(it)
+        D_x = np.zeros((R, N_x), dtype=int)
+        D_z = np.zeros((R, N_z), dtype=int)
+        for i in range(R):
+            inds = np.random.choice(N_x, size=a_x, replace=False)
+            inds2 = np.random.choice(N_z, size=a_z, replace=False)
+            D_x[i, inds] = 1
+            D_z[i, inds2] = 1
+
+        for k, ayi in enumerate(a_y_range):
+            # print(ayi)
+            D_y = np.zeros((R, N_y), dtype=int)
+            w = generate_random_matrix(N_y, N_x, a_w)
+            w_yz = np.zeros((N_z, N_y))
+            w_xz = np.zeros((N_z, N_x))
+            for i in range(R):
+                D_y[i] = kWTA(w @ D_x[i], ayi)
+                w_yz += np.outer(D_z[i], D_y[i])
+                w_xz += np.outer(D_z[i], D_x[i])
+
+
+            w_yz[w_yz > 0] = 1
+            w_xz[w_xz > 0] = 1
+
+            #Recall
+            error = np.zeros(R)
+            error2 = np.zeros(R)
+            max_error = 2 * a_z * (1 - float(a_z) / N_z) # for random retrieval
+            # print(max_error)
+            noise = 0.0
+            for i in range(R):
+                if noise > 0:
+                    z = kWTA(w_yz @ (D_y[i] ^ np.random.binomial(1, noise, size=N_y)), a_z)
+                    z2 = kWTA(w_xz @ (D_x[i] ^ np.random.binomial(1, noise, size=N_x)), a_z)
+                else:
+                    z = kWTA(w_yz @ D_y[i], a_z)
+                    z2 = kWTA(w_xz @ D_x[i], a_z)
+                error[i] = np.sum(np.abs(D_z[i] - z)) / (max_error)
+                error2[i] = np.sum(np.abs(D_z[i] - z2)) / (max_error)
+
+            data[it, k] = np.mean(error)
+            data2[it, k] = np.mean(error2)
+        # print(error)
+        # print(np.mean(error))
+    # print(D_y[:3])
+    # print(data)
+    # print(time.time() - t)
+
+
+    plt.plot(a_y_range / N_y, np.mean(data, axis=0), '-.', label=f'R={R} with expansion')
+    plt.plot(a_y_range / N_y, np.mean(data2, axis=0), '--', label=f'R={R} without hidden layer')
+    print(f'R={R}')
+    print('min error', np.min(np.mean(data, axis=0)))
+    print('a_min', a_y_range[np.argmin( np.mean(data, axis=0))])
+    print('s_min', a_y_range[np.argmin( np.mean(data, axis=0))]/N_y)
+
+
+plot_curve_both(100)
+plot_curve(200)
+plot_curve(200, noise=0.01)
+
+plt.legend()
+plt.ylim([0, 1.1])
+plt.xlim([0, 0.2])
+plt.xticks(np.arange(0, 0.2, step=0.05))
+plt.xlabel(r'$s_y$')
+plt.ylabel(r'Scaled retrieval error')
+plt.legend()
+plt.savefig('figures/association_all', bbox_inches='tight')
+plt.show()
 
